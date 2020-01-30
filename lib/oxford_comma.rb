2d5daf251a4df78aa354268135445aa5b9319bfc
Jesse Griffin <jesse.griffin@gmail.com>
2020-01-30 05:12:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
  if array.size == 1 
    array.join
- else array.size == 2 
+ elsif array.size == 2 
    array.join(" and ")
- #else array.size >= 3
+ else array.size >= 3
    array.last.insert(0, "and ")
    array.join(", ")
  end
